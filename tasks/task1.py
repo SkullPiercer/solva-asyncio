534ed456a4072aeafe5e3cf83fc712bd43cd30a2
Александр Денисов <alde@efin.kz>
2025-05-27 15:14:55 +0500

    Первое задание готово. Обновил зависимости

---
+++ tasks/task1.py
@@ -0,0 +1,14 @@
+# ЗАДАЧА 1
+# Напишите корутину fetch_status(), которая отправляет GET-запрос по URL и возвращает HTTP-статус ответа.
+# Используйте aiohttp.ClientSession и ключевое слово await.
+# Затем создайте функцию fetch_all(), которая принимает список URL и запускает параллельно несколько запросов.
+# Верните список статусов по каждому URL.
+
+import aiohttp
+import asyncio
+
+async def fetch_status(session, url):
+    pass
+
+async def fetch_all(urls):
+    pass
